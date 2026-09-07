148b926dc633bea4465c70218bda673f9cd88c12
MkhitaryanAshot23 <52845668+MkhitaryanAshot23@users.noreply.github.com>
2026-09-07 21:17:08 +0700

    cmake: expose isolated TQ3_4S intdot bench target

---
+++ cmake/tq3-intdot-bench.cmake
@@ -0,0 +1,5 @@
+if (GGML_SYCL AND NOT TARGET test-tq3-4s-intdot-bench)
+    add_executable(test-tq3-4s-intdot-bench "${PROJECT_SOURCE_DIR}/tests/test-tq3-4s-intdot-bench.cpp")
+    target_include_directories(test-tq3-4s-intdot-bench PRIVATE "${PROJECT_SOURCE_DIR}/ggml/src")
+    target_link_libraries(test-tq3-4s-intdot-bench PRIVATE ggml)
+endif()
